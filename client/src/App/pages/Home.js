ed864964af32085edf7dd5de697eb9d69bc8b37f
sapress <sapress@iu.edu>
2020-07-12 21:55:12 -0500

    Home.js

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
 class Home extends Component {
-  render() {
-    return (
-    <div className="App">
-      <h1>Project Home</h1>
-      {/* Link to List.js */}
-      <Link to={'./list'}>
-        <button variant="raised">
-            My List
-        </button>
-      </Link>
-    </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Project Home</h1>
+                {/* Link to List.js */}
+                <Link to={'./list'}>
+                    <button variant="raised">My List</button>
+                </Link>
+                <div>test update</div>
+            </div>
+        );
+    }
 }
 export default Home;
