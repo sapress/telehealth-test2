702abf9a99f364169ea193d298315bb9faee0c25
sapress <sapress@iu.edu>
2020-07-13 00:36:13 -0500

    community attempt 2

--- client/src/App/pages/CommunityHome.js
+++ client/src/App/pages/CommunityHome.js
@@ -4,6 +4,9 @@
 // etc. TODO
 // - overall encourages support group formation
 
+import React from 'react';
+import { Component } from 'react';
+
 const { render } = require('react-dom');
 
 class CommunityHome extends React.Component {
