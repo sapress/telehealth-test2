9ee7a31bd30fa77e7d2bd8641d6aea3fc112e958
sapress <sapress@iu.edu>
2020-07-12 23:08:17 -0500

    fourth attempt with redux

--- client/src/App/reducers/authReducer.js
+++ client/src/App/reducers/authReducer.js
@@ -1,15 +1,25 @@
-const INITIAL_STATE = {
-    const isSignedIn = null,
-    userID = null,
-}
+// const INITIAL_STATE = {
+//     const isSignedIn = null,
+//     userID = null,
+// }
 
+// export default (state = INITIAL_STATE, action) => {
+//     switch (action.type) {
+//         case SIGN_IN:
+//             return { ...state, isSignedIn: true, userId: action.payload };
+//         case SIGN_OUT:
+//             return { ...state, isSignedIn: false, userId: null };
+//         default:
+//             return state;
+//     }
+// };
 
-export default (state = INITIAL_STATE, action) => {
+export default (action) => {
     switch (action.type) {
         case SIGN_IN:
-            return { ...state, isSignedIn: true, userId: action.payload };
+            return state;
         case SIGN_OUT:
-            return { ...state, isSignedIn: false, userId: null };
+            return state;
         default:
             return state;
     }
