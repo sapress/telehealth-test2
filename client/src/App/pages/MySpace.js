a82d5a5081d68892bfe575257e97ed2397e4a3eb
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-14 14:09:21 -0500

    Add my page

---
+++ client/src/App/pages/MySpace.js
@@ -0,0 +1,28 @@
+//Page to take user to journaling suite or meditation resources
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
+
+class MySpace extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>My Personal Space</h1>
+
+                {/* Link to List.js */}
+                <Link to={'./list'}>
+                    <button variant="raised">My Journal</button>
+                </Link>
+				
+				{/* Link to List.js */}
+                <Link to={'./list'}>
+                    <button variant="raised">Practice Meditation</button>
+                </Link>
+				
+				
+            </div>
+        );
+    }
+}
+export default MySpace;
