dafde63c3226717b27a432914aa9717711b4b86d
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-14 15:32:07 -0500

    Making it even simpler

--- client/src/App/pages/JournalForm.js
+++ client/src/App/pages/JournalForm.js
@@ -1,53 +1,19 @@
-import React, { Component } from 'react';
+// Landing page for functionality allowing students to:
+// message other students (anon or no),
+// form groups (group messaging),
+// etc. TODO
+// - overall encourages support group formation
 
-class JournalForm extends Component {
-  // Initialize the state
-  constructor(props){
-    super(props);
-    this.state = {
-      list: []
+import React from 'react';
+import { Component } from 'react';
+
+const { render } = require('react-dom');
+
+class JournalForm extends React.Component {
+    render() {
+        return <div>Welcome to your journal.</div>;
+		return <div>What would you like to do?<\div>;
     }
-  }
-
-  // Fetch the list on first mount
-  componentDidMount() {
-    this.getList();
-  }
-
-  // Retrieves the list of items from the Express app
-  getList = () => {
-    fetch('/api/getList')
-    .then(res => res.json())
-    .then(list => this.setState({ list }))
-  }
-
-  render() {
-    const { list } = this.state;
-
-    return (
-      <div className="App">
-        <h1>List of Items</h1>
-        {/* Check to see if any items are found*/}
-        {list.length ? (
-          <div>
-            {/* Render the list of items */}
-            {list.map((item) => {
-              return(
-                <div>
-                  {item}
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          <div>
-            <h2>No List Items Found</h2>
-          </div>
-        )
-      }
-      </div>
-    );
-  }
 }
 
 export default JournalForm;
