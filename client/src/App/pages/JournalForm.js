5a357c4618b09d769c15fed932dc5bd9d9cf84a1
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-14 15:06:11 -0500

    Modified form

--- client/src/App/pages/JournalForm.js
+++ client/src/App/pages/JournalForm.js
@@ -1,3 +1,8 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
+
+
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
