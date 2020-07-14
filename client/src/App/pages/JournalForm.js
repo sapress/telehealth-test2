664ad4d0b298b95b8532e7ee5836b0f1dd8f7774
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-14 15:53:37 -0500

    Tweak JournalForm again

--- client/src/App/pages/JournalForm.js
+++ client/src/App/pages/JournalForm.js
@@ -1,18 +1,28 @@
-// Landing page for functionality allowing students to:
-// message other students (anon or no),
-// form groups (group messaging),
-// etc. TODO
-// - overall encourages support group formation
+//Page to take user to journaling suite or meditation resources
 
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 
-const { render } = require('react-dom');
-
-class JournalForm extends React.Component {
+class MySpace extends Component {
     render() {
-        return <div>Welcome to your journal.</div>;
+        return (
+            <div className="App">
+                <h1>Journal Form</h1>
+
+                {/* Link to List.js */}
+                <Link to={'./list'}>
+                    <button variant="raised">Create New Entry</button>
+                </Link>
+				
+				{/* Link to List.js */}
+                <Link to={'./list'}>
+                    <button variant="raised">Read Old Entries</button>
+                </Link>
+				
+				
+            </div>
+        );
     }
 }
-
 export default JournalForm;
