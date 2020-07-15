0220faeff4ff9ead7fa212df83c9c7e78d0e167a
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-15 13:02:33 -0500

    More button stuff

--- client/src/App/pages/JournalPrompts.js
+++ client/src/App/pages/JournalPrompts.js
@@ -4,24 +4,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { NavLink, withRouter } from 'react-router-dom';
 
-function JournalPrompts() {
-  const users = ['James', 'Nora', 'Matthew', 'Joe', 'Susan'];
-  
-  function deleteUserWithName(name) {
-    ...
-  }
-  
-  return (
-    <ul>
-      {users.map(name => (
-        <li>
-          <button onClick={() => deleteUserWithName(name)}>
-            Click me!
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
+class JournalHome extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Please Select one of the Following Journal Prompts</h1>
+
+                <div>
+				
+				<ul> <li onClick={() => console.log("clicked A!")}>A</li> </ul>
+				<ul> <li onClick={() => console.log("clicked B!")}>B</li> </ul>
+				<ul> <li onClick={() => console.log("clicked C!")}>C</li> </ul>
+				<ul> <li onClick={() => console.log("clicked D!")}>D</li> </ul>
+				
+				</div>
+				
+				
+            </div>
+        );
+    }
 }
+
+
 
 export default JournalPrompts;
