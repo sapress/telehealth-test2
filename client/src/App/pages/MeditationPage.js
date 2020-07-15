c12b4eb6bbbadcbde46ccbbdedb45c77b238b294
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-15 11:11:58 -0500

    Added to meditation page

--- client/src/App/pages/MeditationPage.js
+++ client/src/App/pages/MeditationPage.js
@@ -16,7 +16,18 @@ class MeditationPage extends React.Component {
 			<section>
 			<h2>Audio Resources for Guided Meditation</h2>
 			<div>
-				<a href="https://www.mindful.org/5-guided-meditations-to-investigate-panic-and-anxiety/" target="_blank">Guided Meditation for Panic and Anxiety (credit: mindful.org)</a>
+			<dl>
+				<dt><a href="https://www.mindful.org/5-guided-meditations-to-investigate-panic-and-anxiety/" target="_blank">Guided Meditation for Panic and Anxiety (credit: mindful.org)</a></dt>
+					<dd>This resouce contains:</dd>
+					<dd> - 30-Minute Meditation to Calm Panic (Bob Stahl) </dd>
+					<dd> - 16-Minute Meditation to Create Space (Jessica Morey)</dd>
+					<dd> - 20-Minute Anxiety Practice (Hugh Byrne)</dd>
+					<dd> - 10-Minute Meditation for Difficult Emotions (Carley Hauck)</dd>
+					<dd> - 20-Minute Meditation on Anxious Emotions (Bob Stahl)</dd>
+			</dl>
+			</div>
+			<div>
+				Don't have that much time? Click <a href="https://www.mindful.org/5-guided-meditations-to-investigate-panic-and-anxiety/" target="_blank"><b>here</b></a> for a collection of guided meditations 5 minutes or less.
 			</div>
 			</section>
 		
