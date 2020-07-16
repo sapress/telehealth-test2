42d7cfadd8425ccd316a875ef45f2c31f4a72063
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-16 13:50:15 -0500

    Mod Prompts

--- client/src/App/pages/JournalPrompts.js
+++ client/src/App/pages/JournalPrompts.js
@@ -4,41 +4,41 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { NavLink, withRouter } from 'react-router-dom';
 
-
-
-function PromptOne(){
-	var WhichPrompt = 'Prompt One';
-	sessionStorage.WhichPrompt = JSON.stringify(WhichPrompt);
-	alert('Prompt One!');
+function handleChange(evt) {
+  const value = evt.target.value;
+  setState({
+    ...state,
+    [evt.target.name]: value
+  });
 }
 
-function PromptTwo(){
-	var WhichPrompt = 'Prompt Two';
-	sessionStorage.WhichPrompt = JSON.stringify(WhichPrompt);
-	alert('Prompt Two!');
-}
-
-
-class JournalPrompts extends React.Component {
-	render() {
-		return(
-			<div className="App">
-
-
-				<h1>Please Select one of the Prompts Below</h1>
-
-			<div>
-				<select>
-  <option value="grapefruit">Grapefruit</option>
-  <option value="lime">Lime</option>
-  <option selected value="coconut">Coconut</option>
-  <option value="mango">Mango</option>
-</select>
-			</div>
-			</div>
-			
-		);
-	}
+function Form() {
+  const [state, setState] = React.useState({
+    firstName: "",
+    lastName: ""
+  })
+  return (
+    <form>
+      <label>
+        First name
+        <input
+          type="text"
+          name="firstName"
+          value={state.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Last name
+        <input
+          type="text"
+          name="lastName"
+          value={state.lastName}
+          onChange={handleChange}
+        />
+      </label>
+    </form>
+  );
 }
 
 export default JournalPrompts;
