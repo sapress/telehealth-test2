5c5f99c7c2486cff93e570689a03504cae6927bc
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-16 14:01:26 -0500

    Prompt stuff

--- client/src/App/pages/JournalPrompts.js
+++ client/src/App/pages/JournalPrompts.js
@@ -4,41 +4,56 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { NavLink, withRouter } from 'react-router-dom';
 
-function handleChange(evt) {
-  const value = evt.target.value;
-  setState({
-    ...state,
-    [evt.target.name]: value
-  });
-}
 
-function Form() {
-  const [state, setState] = React.useState({
-    firstName: "",
-    lastName: ""
-  })
-  return (
-    <form>
-      <label>
-        First name
-        <input
-          type="text"
-          name="firstName"
-          value={state.firstName}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Last name
-        <input
-          type="text"
-          name="lastName"
-          value={state.lastName}
-          onChange={handleChange}
-        />
-      </label>
-    </form>
-  );
+
+
+
+//alert('Prompt Two!');
+
+class JournalPrompts extends React.Component {
+	
+	 constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+			
+	
+	render() {
+		return(
+			<div className="App">
+
+
+				<h1>Please Select one of the Prompts Below</h1>
+
+			      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+	  
+			</div>
+			
+		);
+	}
 }
 
 export default JournalPrompts;
