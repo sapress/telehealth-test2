94540fe348f83a6ec99f19800ac22e7aef9d80f5
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-22 12:18:21 -0500

    Trying to start multiple scripts

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -22,23 +18,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.use(cors());
-app.use(express.json());
+const port = process.env.PORT || 5000;
+app.listen(port);
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
-
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+console.log('App is listening on port ' + port);
