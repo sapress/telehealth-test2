02eb6b9d1e90bac643add593189d4f6a459c6e83
danielatvalpo <45440753+danielatvalpo@users.noreply.github.com>
2020-07-22 12:10:52 -0500

    Mod index

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -18,7 +22,23 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+app.use(cors());
+app.use(express.json());
 
-console.log('App is listening on port ' + port);
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
